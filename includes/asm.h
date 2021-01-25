85a0a5961feeed0b0bd632d423f09059e7c2f3ae
Christopher <csphilli@gmail.com>
2021-01-25 20:28:18 +0200

    added get_name prototype

--- includes/asm.h
+++ includes/asm.h
@@ -6,7 +6,7 @@
 /*   By: cphillip <cphillip@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/09 17:29:14 by cphillip          #+#    #+#             */
-/*   Updated: 2021/01/25 15:58:06 by cphillip         ###   ########.fr       */
+/*   Updated: 2021/01/25 19:24:05 by cphillip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,20 @@
 # include "op.h"
 #include <stdio.h> // remove before submission
 
-typedef struct 	s_input
+typedef struct 		s_input
 {
-	int			index;
-	char 		*line;
-}				t_input;
+	int				index;
+	char 			*line;
+}					t_input;
 
-void	print_hex(char *buf);
-void	print_bytecode(char *bytecode);
+typedef struct		s_champ
+{
+	char			prog_name[PROG_NAME_LENGTH + 1];
+	unsigned int	prog_size;
+	char			comment[COMMENT_LENGTH + 1];
+	unsigned int	magic;
+}					t_champ;
+
+t_input 	*get_data(t_list *list, char **av);
+int			get_name(t_list *list, t_champ *champ, int i);
 #endif
